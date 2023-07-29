e9b63e77dbb13a3bfabaac9cb764b212c9e71d80
Rahul Gupta <rahul.gupta.ce@gmail.com>
2023-07-30 02:28:40 +0530

    Fix-2

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world! This is Rahul's Remote Control using Ansible");
+res.end("Hello World! This is Config and Deployment");
 });
 server.listen(3000);
